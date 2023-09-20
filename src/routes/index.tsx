4563fb871222f700afd709f48dc6c23706f29f0c
Yaël Busser <yael.busser@gmail.com>
2023-09-20 15:15:14 +0200

    test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 export default function RouterComponent() {
 
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
         </Routes>
     );
 }
