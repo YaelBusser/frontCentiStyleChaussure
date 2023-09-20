318ecaa1cd31efe30407b378d60657cac2b07f0c
Yaël Busser <yael.busser@gmail.com>
2023-09-20 15:07:24 +0200

    add project front

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from "react";
+import RouterComponent from "./routes";
+function App() {
+  return (
+    <>
+      <RouterComponent/>
+    </>
+  )
+}
+
+export default App
